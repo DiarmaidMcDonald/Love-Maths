c7431cf25009d5a7ca72b741bba787b7d31954c7
DiarmaidMcDonald <>
2023-07-13 15:19:07 +0000

    Set up structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function runGame() {
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswers() {
+
+}
+
+function displayAddtionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+    
+}
