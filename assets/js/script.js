d7b3e31e48fdd691605c1bdea58a54a56c285b38
DiarmaidMcDonald <>
2023-07-15 12:14:50 +0000

    Add code to check the answer and respond to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentListener", function () ) {
     for (let button of buttons) {
         button.addEventListener("click", fucntion() ) {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -37,13 +37,44 @@ function runGame(gameType) {
         throw `Unknown game type: ${gameType}. Aborting!`;
         }
     }
-}
+
+    /**
+     * Check the answer against the first element in
+     * the returned calculateCorrectAnswer array
+     */
 
 function checkAnswer() {
 
-}
-function calculateCorrectAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculateCorrectAnswer[0];
 
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculateCorrectAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
+
+}
+
+/**
+ * Get the operands (the numbers) and the operators (plus, mius, etc)
+ * directly from the DOM, and returns the correct answer.
+ */
+function calculateCorrectAnswer() {
+      let operand1 = parseInt(documnet.getElementById('operand1').innerText);
+      let operand2 = parseInt(documnet.getElementById('operand2').innerText);
+      let operator = documnet.getElementById("operator").innerText;
+
+      if(operator === "+") {
+        return[operand1 + operand2, "addition"];
+      } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+      }
+      }
 }
 
 function incrementScore() {
